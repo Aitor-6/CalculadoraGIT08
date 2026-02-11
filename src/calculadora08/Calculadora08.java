98b892e695f3ebe184a430a94be0186616792ecd
unknown <aitor.fernandez07@somo.eus>
2026-02-11 21:30:57 +0100

    Proyecto CalculadoraGIT08 clase Calculadora y método multiplicar

--- src/calculadora08/Calculadora08.java
+++ src/calculadora08/Calculadora08.java
@@ -9,13 +9,12 @@ public class Calculadora08 {
 	public int restar(int a, int b) {
 		 return a - b;
 	}
-	
-	public int multiplicar(int a, int b) {
-		 return a * b;
-	}
 		
 	public double dividir(int a, int b) {
 		 return (double) a / b;
 	}
 
+	public int multiplicar(int a, int b) {
+		 return a * b;
+	}
 }
