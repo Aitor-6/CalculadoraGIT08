4dfe77fdf751f8e0d6fd597d6ceeb981db8bdcc5
unknown <aitor.fernandez07@somo.eus>
2026-02-11 21:34:48 +0100

    Hemos controlado la división entre cero

--- src/calculadora08/Calculadora08.java
+++ src/calculadora08/Calculadora08.java
@@ -11,8 +11,12 @@ public class Calculadora08 {
 	}
 		
 	public double dividir(int a, int b) {
+		 if (b == 0) {
+		 throw new IllegalArgumentException("No se puede dividir entre cero");
+		 }
 		 return (double) a / b;
 	}
+
 
 	public int multiplicar(int a, int b) {
 		 return a * b;
